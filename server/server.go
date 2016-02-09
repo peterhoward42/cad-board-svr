830d240a7445ca94cbba09fbb6c4bd8bbc4ee081
peterhoward42 <peterhoward42@gmail.com>
2016-02-09 16:16:59 +0000

    Provide handler to receive mouse x coordinates and store them in memcache.

--- server/server.go
+++ server/server.go
@@ -1,20 +1,47 @@
 package server
 
 import (
-    "html/template"
-    "net/http"
+	"html/template"
+	"net/http"
+	"appengine"
+	"appengine/memcache"
+	"appengine/log"
 )
 
 func init() {
-    http.HandleFunc("/", handler)
+	http.HandleFunc("/", mainPageHandler)
+	http.HandleFunc("/mouse", mouseHandler)
 }
 
-func handler(w http.ResponseWriter, r *http.Request) {
-    parsed_template, err := template.ParseFiles("static/template/index.html")
+func mainPageHandler(w http.ResponseWriter, r *http.Request) {
+	parsed_template, err := template.ParseFiles("static/template/index.html")
 	// how to use gui name in template exec fn
 	parsed_template.Execute(w, gui_data())
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
+
+func mouseHandler(w http.ResponseWriter, r *http.Request) {
+	// extract the mouse x coordinate sent
+	x := r.FormValue("xcoord")
+	ctx := appengine.NewContext(r)
+	ctx.Infof("request contains x coord string: %v", x)
+	storeXCoordInMemcache(ctx, x);
+}
+
+func storeXCoordInMemcache(ctx appengine.Context, x string) void {
+	newMemcacheItem := &memcache.Item{Key: "mouse_x", Value: []byte(x)}
+	// Add the item to the cache (if not already present)
+	err := memcache.Add(ctx, newMemcacheItem)
+	// Ignore error that is already present in cache, but react to other errors
+	if err != memcache.ErrNotStored {
+		ctx.Infof("error adding item: %v", err)
+	}
+	// Now overwrite the cached item regardless
+	err = memcache.Set(ctx, newMemcacheItem)
+	if err {
+		ctx.Errorf(ctx, "error setting item: %v", err)
 	}
 }
 
