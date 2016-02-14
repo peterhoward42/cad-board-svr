8eb5b54440c52744c80366149a942ae708172909
peterhoward42 <peterhoward42@gmail.com>
2016-02-14 16:13:02 +0000

    Implemented lossy, back pressure throttling of mouse movement POSTS to server.

--- static/js/mouseEventTransmitter.js
+++ static/js/mouseEventTransmitter.js
@@ -1,14 +1,20 @@
 /*
 This object will track mouse movement events from an given html5 element, and relay them
-with a POST message to a an http server. (asynchronously).
+with POST messages to a an http server. (asynchronously).
 
 The constructor takes care of registering itself as an event handler for the html element you
 specify.
 
-It takes some steps to optimise the communications. Firstly it ignores small mouse movements.
+It takes some steps to optimise the communications assuming that the incoming mouse event stream
+would far exceed how fast we can send them to the server. Firstly it ignores small mouse movements.
 Secondly it throttles its sending of messages to match the rate at which the server can receive
-and reply to them. Any mouse movements it receives while waiting for the server to be ready are
-discarded, so the server receives the most recently available position always.
+and reply to them. It does this using a "lossy" back-pressure strategy, whereby a maximum of <N>
+POSTS are in flight at any one time. Mouse movements received while the pipe has backed up are
+"spilled", but we keep track of the most recently received only, and arranging for that one to be
+flushed as soon as the POST's reply callback detects room in the pipe. In the steady
+throttled  state, this means we are sending events at the maximum rate our communication with the
+server can handle, but we get the benefit of sending messages concurrently without waiting for
+the previous message's round trip.
 */
 
 // Factory method for the object.
@@ -23,11 +29,13 @@ function mouseEventTransmitter(mouseEventSource) {
     t.serverUrl = "/mouse";
 
     t.negligibleMovementSize = 5;
+    t.maxInFlight = 3; // The <N> in the description above.
 
     // State
     t.prevX = 1e6; // An out of band sentinel value.
     t.prevY = 1e6;
-    t.waitingForServerReply = false;
+    t.numberOfMessagesInFlight = 0;
+    t.mostRecentUnSentPosition = null;
 
     // Functions
 
@@ -35,20 +43,30 @@ function mouseEventTransmitter(mouseEventSource) {
     t.handleMoveEvent= function(event) {
         mouseX = event.clientX - t.left;
         mouseY = event.clientY - t.top;
-        // Optimise out moves that are too small (or zero) while still in integer coordinates
-        if (t.movementTooSmall(mouseX, mouseY))
+        // Optimise out, and ignore moves that are too small (or zero) while still in integer
+        // coordinates
+        if (t.movementTooSmall(mouseX, mouseY)) {
             return;
-        t.prevX = mouseX;
+        }
+        t.prevX = mouseX; // used to test for too small
         t.prevY = mouseY;
 
-        // Throttle the message sending rate to match the server's replies.
-        if (t.waitingForServerReply)
-            return;
-
-        // Send the new position to the server
         normalisedX = mouseX / t.width;
         normalisedY = mouseY / t.height;
-        t.postPositionToServerAsync(normalisedX, normalisedY);
+
+        // Send the position to the server, in accordance with lossy back pressure
+        // strategy.
+
+        // If the pipe is currently full (the general case), all we do is note the most
+        // recently received position, to flush later.
+        if (t.numberOfMessagesInFlight == t.maxInFlight) {
+            t.mostRecentUnSentPosition = {x: normalisedX, y: normalisedY};
+        }
+        else {
+            // Seems we are at liberty to forward the movement, so we do so.
+            t.mostRecentUnSentPosition = null;
+            t.postPositionToServerAsync(normalisedX, normalisedY);
+        }
     };
 
     // Utility function to assess if mouse movement is very small or zero
@@ -64,16 +82,27 @@ function mouseEventTransmitter(mouseEventSource) {
     t.postPositionToServerAsync = function(x, y) {
         payload = {X: normalisedX, Y: normalisedY};
         callbackFn = t.handlerForServerReply;
+        t.numberOfMessagesInFlight += 1;
         $.post(t.serverUrl, payload, callbackFn);
     }
 
     // Handler to accept replies from the server.
     t.handlerForServerReply = function(reply) {
-        console.log("reply from server:", reply);
-        t.waitingForServerReply = false;
+        // Receiving a reply triggers our semaphore maintenance. We are not interested
+        // in the content of the reply.
+        t.numberOfMessagesInFlight -= 1;
+        // There must be some room in the pipe at this point, having just decremented
+        // the counter, so we seize the opportunity to flush the most recently received
+        // position that has not yet been sent, if there is one.
+        if (t.mostRecentUnSentPosition != null) {
+            x = t.mostRecentUnSentPosition.x;
+            y = t.mostRecentUnSentPosition.y;
+            t.mostRecentUnSentPosition = null; // spent
+            t.postPositionToServerAsync(x, y);
+        }
     }
 
-    // Ask the mouse move event emitter to send the events here.
+    // Ask the mouse move event emitter to send the events to a method in this object.
     mouseEventSource.onmousemove = t.handleMoveEvent;
     return t;
 }
